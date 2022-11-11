9d5b45622c781a343b3d067e7463b64b12fa80cf
Gioperes <103082092+Gioperes@users.noreply.github.com>
2022-11-10 19:06:23 -0500

    Adding authentication

--- backend/controllers/authentication.js
+++ backend/controllers/authentication.js
@@ -9,7 +9,7 @@ router.post('/', async (req, res) => {
     let user = await User.findOne({
         where: { email: req.body.email }
     })
-    console.log(user)
+
     if (!user || !await bcrypt.compare(req.body.password, user.passwordDigest)) {
         res.status(404).json({ message: `Could not find a user with the provided username and password` })
     } else {
@@ -18,18 +18,9 @@ router.post('/', async (req, res) => {
     }
 })
 
-router.get('/profile', async (req, res) => {
-    try {
-        let user = await User.findOne({
-            where: {
-                userId: req.session.userId
-            }
-        })
-        res.json(user)
-    } catch {
-        res.json(null)
-    }
-})
 
+router.get('/profile', async (req, res) => {
+    res.json(req.currentUser)
+})
 
 module.exports = router
