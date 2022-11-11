af3f66addf08466de9ae3cb1bfce861ecb2a177d
Gioperes <103082092+Gioperes@users.noreply.github.com>
2022-11-10 19:58:52 -0500

    Adding JWT

--- frontend/src/contexts/CurrentUser.js
+++ frontend/src/contexts/CurrentUser.js
@@ -6,12 +6,13 @@ export const CurrentUser = createContext()
 function CurrentUserProvider({ children }) {
 
     const [currentUser, setCurrentUser] = useState(null)
-
     useEffect(() => {
 
         const getLoggedInUser = async () => {
-            let response = await fetch('http://localhost:5000/authentication/profile', {
-                credentials: 'include'
+            let response = await fetch('http://localhost:3000/authentication/profile', {
+                headers: {
+                    'Authorization': `Bearer ${localStorage.getItem('token')}`
+                }
             })
             let user = await response.json()
             setCurrentUser(user)
