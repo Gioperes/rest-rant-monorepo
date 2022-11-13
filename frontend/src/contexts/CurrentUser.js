41cdc99bf35f3087f9e06447737aa5184014634a
Gioperes <103082092+Gioperes@users.noreply.github.com>
2022-11-13 16:09:24 -0500

    role based authentication

--- frontend/src/contexts/CurrentUser.js
+++ frontend/src/contexts/CurrentUser.js
@@ -6,13 +6,12 @@ export const CurrentUser = createContext()
 function CurrentUserProvider({ children }) {
 
     const [currentUser, setCurrentUser] = useState(null)
+
     useEffect(() => {
 
         const getLoggedInUser = async () => {
             let response = await fetch('http://localhost:3000/authentication/profile', {
-                headers: {
-                    'Authorization': `Bearer ${localStorage.getItem('token')}`
-                }
+                credentials: 'include'
             })
             let user = await response.json()
             setCurrentUser(user)
